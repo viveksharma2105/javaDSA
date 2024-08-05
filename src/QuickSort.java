77f64153eda31c914ca91f8c268dc7137e242e44
vivek sharma <viveksharma02005@gmail.com>
2024-08-05 18:56:57 +0530

    quick-sort

---
+++ src/QuickSort.java
@@ -0,0 +1,42 @@
+public class QuickSort {
+
+    public static int partition(int [] arr, int low, int high){
+        int pivot = arr[high];
+        int i  = low-1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot){
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            
+        }
+        i++;
+        int temp = arr[i];
+        arr[i]  = pivot;
+        arr[high] = temp;
+        return i;
+    }
+    public static void quicksort(int [] arr, int low, int high){
+        if (low < high){
+            int pindx = partition(arr, low, high);
+            quicksort(arr , low, pindx-1);
+            quicksort(arr, pindx+1, high);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {23,23,3,22323,232,323,23232,4};
+        int n  = arr.length;
+
+        quicksort(arr , 0, n-1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+
+    }
+}
